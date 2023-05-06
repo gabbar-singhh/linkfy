3a9d638b06ac3be603d48d1b097250019d471229
dev-himanshu-01 <himanshu01.dev@gmail.com>
2023-05-06 14:00:56 -0700

    dropdown-menu :done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,46 @@ import styles from "./Navbar.module.css";
 import Link from "next/link";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "@/firebase";
-import { Avatar, User } from "@nextui-org/react";
+import {
+  Avatar,
+  User,
+  Dropdown,
+  Text,
+  createTheme,
+  Tooltip,
+} from "@nextui-org/react";
+
+const theme = createTheme({
+  type: "dark", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: "$green200",
+      primaryLightHover: "$green300",
+      primaryLightActive: "$green400",
+      primaryLightContrast: "$green600",
+      primary: "#4ADE7B",
+      orange: "#ff8b37",
+      skyblue: "#7ebdff",
+      primaryBorder: "$green500",
+      primaryBorderHover: "$green600",
+      primarySolidHover: "$green700",
+      primarySolidContrast: "$white",
+      primaryShadow: "$green500",
+
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
+      link: "#5E1DAD",
+
+      // you can also create your own color
+      myColor: "#ff4ecd",
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {},
+  },
+});
 
 const Navbar = () => {
   const GITHUB_REPO_LINK = "https://github.com/dev-himanshu-01/linkify";
@@ -96,16 +135,30 @@ const Navbar = () => {
 
       <div className={styles.RightSideSection}>
         {loginStatus ? (
-          <Avatar
-            onClick={signBtnHandler}
-            size="lg"
-            src={userData.photoURL}
-            style={{ cursor: "pointer" }}
-            name={userData.displayName}
-            color="error"
-            bordered
-            zoomed
-          />
+          <Dropdown>
+            <Dropdown.Trigger className={styles.wrapper}>
+              <img className={styles.photoURL} src={userData.photoURL} />
+            </Dropdown.Trigger>
+            <Dropdown.Menu color="primary" aria-label="User Actions">
+              <Dropdown.Item key="profile" css={{ height: "$18" }}>
+                <Text b color="inherit" css={{ d: "flex" }}>
+                  Signed in as
+                </Text>
+                <Text b color="inherit" css={{ d: "flex" }}>
+                  {userData.email}
+                </Text>
+              </Dropdown.Item>
+              <Dropdown.Item key="user_profile" withDivider>
+                Profile
+              </Dropdown.Item>
+              <Dropdown.Item key="help_and_feedback">
+                Help & Feedback
+              </Dropdown.Item>
+              <Dropdown.Item key="logout" color="error">
+                Log Out
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         ) : (
           <div
             className={`${styles.SignInBtn} shineEff`}
