5658d1e409c556c6876f765346a13f8678386354
dev-himanshu-01 <himanshu01.dev@gmail.com>
2023-05-06 06:03:59 -0700

    retrieving collection from firestore :done

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,29 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useRouter } from "next/router";
 import { db } from "@/firebase";
-import {
-  onSnapshot,
-  getDocs,
-  where,
-  query,
-  collection,
-} from "firebase/firestore";
+import { onSnapshot, getDocs, collection, doc } from "firebase/firestore/lite";
+
+// import { collection } from "firebase/firestore/lite";
 
 const slug = () => {
+  const colRef = collection(db, "links");
+
+  const [data, setData] = useState([]);
   const router = useRouter();
-  const { SLUG } = router.query;
+  const {par} = useParams()
 
   useEffect(() => {
-    const colRef = collection(db, "links");
+    const code = router.query.slug;
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "links"));
+      const dataArray = [];
+      querySnapshot.forEach((doc) => {
+        // console.log('🍌', slug)
+        // console.log('🍎🍎', par);
+        dataArray.push({ code: doc.code, originalURL: doc.originalURL });
 
-    // const q = query(colRef, where("code", "==", SLUG))
+        const CODE_VAL = doc.data().code;
+        const FULL_URL = doc.data().originalURL;
 
-    onSnapshot(colRef, (snapshot) => {
-      snapshot.docs;
-    });
+        if (!slug == CODE_VAL) {
+          console.log("Not Found⛔");
+        } else {
+          console.log("Redirecting Soon✅", par);
+        }
+      });
+      setData(dataArray);
+    };
+    fetchData();
   }, []);
 
-  return <div>{}</div>;
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.code}>
+          {item.originalURL}
+          {""}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default slug;
