40a6433803fb8dc500df19c24c9eb949a986360e
dev-himanshu-01 <himanshu01.dev@gmail.com>
2023-05-06 15:52:09 -0700

    firebase auth :done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,48 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
 import Link from "next/link";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "@/firebase";
-import {
-  Avatar,
-  User,
-  Dropdown,
-  Text,
-  createTheme,
-  Tooltip,
-} from "@nextui-org/react";
-
-const theme = createTheme({
-  type: "dark", // it could be "light" or "dark"
-  theme: {
-    colors: {
-      // brand colors
-      primaryLight: "$green200",
-      primaryLightHover: "$green300",
-      primaryLightActive: "$green400",
-      primaryLightContrast: "$green600",
-      primary: "#4ADE7B",
-      orange: "#ff8b37",
-      skyblue: "#7ebdff",
-      primaryBorder: "$green500",
-      primaryBorderHover: "$green600",
-      primarySolidHover: "$green700",
-      primarySolidContrast: "$white",
-      primaryShadow: "$green500",
-
-      gradient:
-        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
-      link: "#5E1DAD",
-
-      // you can also create your own color
-      myColor: "#ff4ecd",
-
-      // ...  more colors
-    },
-    space: {},
-    fonts: {},
-  },
-});
+import { Dropdown, Text } from "@nextui-org/react";
 
 const Navbar = () => {
   const GITHUB_REPO_LINK = "https://github.com/dev-himanshu-01/linkify";
@@ -53,9 +14,8 @@ const Navbar = () => {
     displayName: "",
     email: "",
     photoURL: "",
+    isLoggedIn: false,
   });
-
-  const [loginStatus, setLoginStatus] = useState(false);
 
   const signBtnHandler = () => {
     const provider = new GoogleAuthProvider();
@@ -79,10 +39,22 @@ const Navbar = () => {
           displayName: user.displayName,
           email: user.email,
           photoURL: user.photoURL,
+          isLoggedIn: true,
         });
 
-        setLoginStatus(true);
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            credential: credential,
+            token: token,
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+            isLoggedIn: true,
+          })
+        );
       })
+
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
@@ -94,6 +66,35 @@ const Navbar = () => {
         const credential = GoogleAuthProvider.credentialFromError(error);
       });
   };
+
+  const menuBtnHandler = (keyVal) => {
+    if (keyVal === "logout") {
+      signOut(auth)
+        .then(() => {
+          setUserData({
+            credential: "",
+            token: "",
+            displayName: "",
+            email: "",
+            photoURL: "",
+            isLoggedIn: false,
+          });
+          localStorage.removeItem("user");
+          console.log("signed out succesfully");
+        })
+        .catch((error) => {
+          // An error happened.
+        });
+    }
+  };
+
+  useEffect(() => {
+
+    const prevSignInDetails = JSON.parse(localStorage.getItem("user"));
+    if (prevSignInDetails){
+      setUserData(prevSignInDetails);
+    }
+  }, []);
 
   return (
     <section className={styles.HeaderSection}>
@@ -134,12 +135,16 @@ const Navbar = () => {
       </div>
 
       <div className={styles.RightSideSection}>
-        {loginStatus ? (
+        {userData.isLoggedIn ? (
           <Dropdown>
             <Dropdown.Trigger className={styles.wrapper}>
               <img className={styles.photoURL} src={userData.photoURL} />
             </Dropdown.Trigger>
-            <Dropdown.Menu color="primary" aria-label="User Actions">
+            <Dropdown.Menu
+              color="primary"
+              aria-label="User Actions"
+              onAction={menuBtnHandler}
+            >
               <Dropdown.Item key="profile" css={{ height: "$18" }}>
                 <Text b color="inherit" css={{ d: "flex" }}>
                   Signed in as
@@ -148,13 +153,13 @@ const Navbar = () => {
                   {userData.email}
                 </Text>
               </Dropdown.Item>
-              <Dropdown.Item key="user_profile" withDivider>
+              <Dropdown.Item textValue="" key="user_profile" withDivider>
                 Profile
               </Dropdown.Item>
-              <Dropdown.Item key="help_and_feedback">
+              <Dropdown.Item textValue="" key="help_and_feedback">
                 Help & Feedback
               </Dropdown.Item>
-              <Dropdown.Item key="logout" color="error">
+              <Dropdown.Item textValue="" key="logout" color="error">
                 Log Out
               </Dropdown.Item>
             </Dropdown.Menu>
