52cbfea6d47130b0eab762e8e1b855279ea89431
dev-himanshu-01 <himanshu01.dev@gmail.com>
2023-05-18 14:00:58 -0700

    [slug] 404-error-page :done && [slug] page-found-animation :added

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { db } from "@/firebase";
 import { getDocs, collection } from "firebase/firestore/lite";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Slug.module.css";
+import Navbar from "@/components/Navbar";
+import { Loading } from "@nextui-org/react";
 
 const slug = () => {
   const router = useRouter();
   const { slug } = router.query;
+  const [showErrorPage, setShowErrorPage] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const querySnapshot = await getDocs(collection(db, "accumulated-data"));
 
       querySnapshot.forEach((doc) => {
-        console.log("🎃", doc.data().originalURL);
-
         if (doc.id == slug) {
           console.log("Redirecting Soon✅", doc.data().originalURL);
           window.location.replace(doc.data().originalURL);
+          setShowErrorPage(false);
+        } else {
+          setShowErrorPage(true);
         }
       });
     };
@@ -26,7 +33,35 @@ const slug = () => {
     }
   }, [slug]);
 
-  return <div>{}</div>;
+  return showErrorPage ? (
+    <>
+      <Head>
+        <title>OOPS! </title>
+      </Head>
+      <Navbar />
+      <section className={styles.Main}>
+        <Image
+          src={"/error-img.webp"}
+          height={350}
+          width={350}
+          alt="robot performing yogo"
+          priority
+        />
+        <div className={styles.Txt}>
+          <p>Oops!</p>
+          <p>Page Not Found</p>
+        </div>
+      </section>
+    </>
+  ) : (
+    <>
+      <section className={styles.Main}>
+        <Loading type="default" size="xl" color="error" textColor="error">
+          Please Wait...
+        </Loading>
+      </section>
+    </>
+  );
 };
 
 export default slug;
