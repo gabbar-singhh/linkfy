a055da97efa746dc6eff60e57aa71375e5d47672
gabbar-singhh <himanshu01.dev@gmail.com>
2025-11-13 03:33:08 +0530

    contact us error

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "components/Navbar.jsx";
+import Navbar from "@/components/Navbar.jsx";
 import Head from "next/head";
 import styles from "@/styles/About.module.css";
 
